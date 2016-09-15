ec4cabb0bd9a313c9161ef7fdf294bbe8a2aabe7
Vaimoana Atonio <vaimoanaatonio@Vaimoanas-MacBook-Pro.local>
2016-09-14 18:41:57 -1000

    Loopy HW

--- loops.js
+++ loops.js
@@ -1,7 +1,16 @@
 /*## `Presidents - Accessing values in an Array`
 Declare a variable named `presidents` which contains the first 5 presidents' last names: `Washington`, `Adams`, `Jefferson`, `Madison`, `Monroe`.*/
 
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+for(var i = 0; i<presidents.length; i++){
+  console.log(presidents[i]);
+  console.log("Value of i is: " + [i]);
+  console.log("Value at " + [i] + " is: " + presidents[i]);
+}
+
 /*Now write a _FOR_ loop which iterates through this Array.
+
 
 Within the _FOR_ loop, use `console.log` to:
 
@@ -10,18 +19,43 @@ Within the _FOR_ loop, use `console.log` to:
 
 /***bonus:** could you take the code inside of this FOR loop and encapsulate it inside of a function called `printContent` and still achieve the same result? This new function should accept two parameters.*/
 
+function printContent(names){
+	for(var i = 0; i<names.length; i++){
+		console.log("Value of i is: ") + [i]);
+		console.log("Value of i is: " ) + [i]);
+		console.log("Value at " + [i] + " is: ")
 
-
+		}
+	}
+	return names;
+} 
+printContent(presidents);
 
 
 /*Declare a variable named `stringOfNumbers` and set it's value to `''` (an empty String).*/
 
+var stringOfNumbers = " ";
+
+
 /*Then write a _FOR_ loop that appends a Number value to that string starting from `10` all the way up to and including `20`.*/
 
+for(var i = 10; i<21; i++){
+	stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
 
 
 /***bonus:** could you take the code inside of this _FOR_ loop and encapsulate it inside of a function called `appendToString` and still achieve the same result?*/
 
+var numbrStr = "";
+
+function appendToString(str){
+	for (var i = 10; i<21; i++){
+		str += i;
+	}
+	return str;
+}
+console.log(appendToString(numberStr));
 
 
 /*## `Add only even numbrs to an array`
@@ -29,7 +63,14 @@ Declare a variable named `evenNumberArray`.
 
 Use a _FOR_ loop to add only even numbers to an Array. Add `50` even numbers to the `evenNumberArray` starting with the value `0`.*/
 
-  
+var evenNumberArray = [];
+
+for(var i = 0; i<99; i++){
+	if(i%2===0){
+		evenNuberArray.push(i);
+	} 
+}
+console.log(evenNumberArray);
 
 /*## `Accessing only the odd indexes of an Array - 'Not Even Brah'`
 Someone forgot to fill out this array! Oh noes...
